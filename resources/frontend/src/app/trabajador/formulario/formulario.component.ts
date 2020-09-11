ab90bfaa0374156c006a090f9123e0a083f77986
Mario Villatoro <ing.mario.villatoro@gmail.com>
2020-09-11 09:00:34 -0500

    actualizacion de formulario trabajador

--- resources/frontend/src/app/trabajador/formulario/formulario.component.ts
+++ resources/frontend/src/app/trabajador/formulario/formulario.component.ts
@@ -28,24 +28,25 @@ export class FormularioComponent implements OnInit {
   ) { }
   
   trabajadorForm = this.fb.group({
+   
     'nombre': ['',[Validators.required]],
     'apellido_paterno': [''],
     'apellido_materno': [''],
     'rfc': ['',[Validators.required]],
     'curp': ['',[Validators.required]],
-    'pais_nacimiento': ['',[Validators.required]],
-    'entidad_nacimiento': ['',[Validators.required]],
-    'municipio_nacimiento': ['',[Validators.required]],
-    'nacionalidad': ['',[Validators.required]],
+    'pais_nacimiento_id': ['',[Validators.required]],
+    'entidad_nacimiento_id': ['',[Validators.required]],
+    'municipio_nacimiento_id': ['',[Validators.required]],
+    'nacionalidad_id': ['',[Validators.required]],
     'fecha_nacimiento': ['',[Validators.required]],
     'edad': ['',[Validators.required]],
-    'estado_conyugal': ['',[Validators.required]],
+    'estado_conyugal_id': ['',[Validators.required]],
     'sexo': ['',[Validators.required]],
     'telefono_fijo': ['',[Validators.required]],
     'telefono_celular': ['',[Validators.required]],
-    'correo_electronico': ['',[Validators.required]],
+    'correo_electronico': ['',[Validators.required, Validators.email]],
     'calle': ['',[Validators.required]],
-    'no_interior': ['',[Validators.required]],
+    'no_interior': [''],
     'no_exterior': ['',[Validators.required]],
     'colonia': ['',[Validators.required]],
     'cp': ['',[Validators.required]],
@@ -87,12 +88,73 @@ export class FormularioComponent implements OnInit {
     //Datos escolares
     'nivel_maximo_id':[],
 
+    //Cursos
+    'tipo_ciclo_formacion_id':[],
+    'carrera_ciclo_id':[],
+    'institucion_ciclo_id':[],
+    'anio_cursa_id':[],
+    'colegiacion':[],
+    'colegio_id':[],
+    'certificacion':[],
+    'certificacion_id':[],
+    'consejo':[],
+    'idioma_id':[],
+    'nivel_idioma_id':[],
+    'lengua_indigena_id':[],
+    'nivel_lengua_id':[],
+    'lenguaje_senias':[],
+
+    //capacitacion
+    'capacitacion_anual':[],
+    'grado_academico_id':[],
+    'titulo_diploma_id':[],
+    'otro_nombre_titulo':[],
+    'institucion_id':[],
+    'otro_nombre_institucion':[],
+    'ciclo_id':[],
+
+
   });
 
+  displayedColumns: string[] = ['tipo','descripcion','institucion','cedula','actions'];
+  displayedColumnsCursos: string[] = ['entidad','nombre_curso','actions'];
+  dataSource: any = [];
+  dataSourceCapacitacion: any = [];
   
 
   ngOnInit() {
-    
+    this.trabajadorForm.patchValue({fecha_nacimiento: new Date('1987/01/28')}); 
+  }
+
+  verificar_curp(curp):void
+  {
+    if(curp.length == 18)
+    {
+      this.trabajadorForm.patchValue({rfc: curp.substring(0, 10)}); 
+      let sexo = curp.substring(10,11);
+      let estado = curp.substring(11,13);
+      let fecha_nacimiento = curp.substring(4,10);
+      
+      let anio = fecha_nacimiento.substring(0,2);
+      let mes = fecha_nacimiento.substring(2,4);
+      let dia = fecha_nacimiento.substring(4,6);
+      
+      this.trabajadorForm.patchValue({fecha_nacimiento: new Date(anio+"-"+mes+"-"+dia)}); 
+      
+      if(sexo == "H")
+      {
+
+      }else if(sexo == "F")
+      {
+
+      }
+      if(estado == "NE")
+      {
+
+      }else{
+
+      }
+    }
   }
 
 }
