830fd993e3368b20640ef4097cc8a56aef983721
Mario Alberto Cabrera Alfaro <maca.15c@gmail.com>
2021-03-11 14:34:33 -0600

    angular updates

--- resources/frontend/src/app/trabajador/lista/lista.component.ts
+++ resources/frontend/src/app/trabajador/lista/lista.component.ts
@@ -372,6 +372,10 @@ export class ListaComponent implements OnInit {
     this.loadTrabajadorData(null);
   }
 
+  cleanSearch(){
+    this.searchQuery = '';
+  }
+
   cleanFilter(filter){
     filter.value = '';
     //filter.closePanel();
