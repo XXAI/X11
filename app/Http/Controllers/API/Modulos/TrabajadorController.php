d805f2969b0e1be7f88d57852ec6406fd89911dc
Mario Alberto Cabrera Alfaro <maca.15c@gmail.com>
2021-08-03 22:56:15 -0500

    correcion en index de trabajadores

--- app/Http/Controllers/API/Modulos/TrabajadorController.php
+++ app/Http/Controllers/API/Modulos/TrabajadorController.php
@@ -78,8 +78,8 @@ class TrabajadorController extends Controller
             $parametros = $request->all();
             $trabajador = Trabajador:://with("datoslaborales")//select('trabajador.*')
                             join("rel_trabajador_datos_laborales", "rel_trabajador_datos_laborales.trabajador_id", "=", "trabajador.id")
-                            ->leftjoin("rel_trabajador_datos_laborales_nomina", "rel_trabajador_datos_laborales_nomina.trabajador_id", "=", "trabajador.id")
-                            ->select("trabajador.*", "rel_trabajador_datos_laborales.cr_fisico_id", "rel_trabajador_datos_laborales_nomina.cr_nomina_id")
+                            ->leftjoin("rel_trabajador_datos_laborales_nomina as datos_nominales", "datos_nominales.trabajador_id", "=", "trabajador.id")
+                            ->select("trabajador.*", "rel_trabajador_datos_laborales.cr_fisico_id", "datos_nominales.cr_nomina_id")
                             ->whereRaw("trabajador.id not in (select trabajador_id from rel_trabajador_baja)");
                             //
                            
