6243834f61c6760b46a9c5e7fd3917250c6b0a19
Mario Villatoro <ing.mario.villatoro@gmail.com>
2022-08-31 11:21:13 -0500

    actualizacionn de comision interna, se quita error

--- app/Http/Controllers/API/Modulos/CredencializacionController.php
+++ app/Http/Controllers/API/Modulos/CredencializacionController.php
@@ -44,7 +44,8 @@ class CredencializacionController extends Controller
             $parametros = $request->all();
             $trabajador = Trabajador::with("rel_datos_comision", "rel_datos_laborales", "credencial")
                             ->whereRaw("trabajador.id not in (select trabajador_id from rel_trabajador_comision where tipo_comision_id='CS' and fecha_fin>=".$carbon->format('Y-m-d')." and estatus='A' )")
-                            ->whereRaw("trabajador.id not in (select trabajador_id from rel_trabajador_baja where tipo_baja_id=2 and fecha_fin_baja='0000-00-00' and deleted_at is null)");
+                            ->whereRaw("trabajador.id not in (select trabajador_id from rel_trabajador_baja where tipo_baja_id=2 and fecha_fin_baja='0000-00-00' and deleted_at is null)")
+                            ->whereRaw("trabajador.id not in (select trabajador_id from rel_trabajador_baja where deleted_at is not null)");
                             //->join("rel_trabajador_datos_laborales", "rel_trabajador_datos_laborales.trabajador_id", "=", "trabajador.id");
                             //->leftjoin("rel_trabajador_datos_laborales_nomina as datos_nominales", "datos_nominales.trabajador_id", "=", "trabajador.id")
                             //->select("trabajador.*", "rel_trabajador_datos_laborales.cr_fisico_id", "datos_nominales.cr_nomina_id")
