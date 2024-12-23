a927a8c7f821c45ba6b4faa9c9331a21a6c09cc9
mariov <ing.mario.villatoro@gmail.com>
2024-12-23 15:56:04 -0600

    build de credenciales

--- resources/frontend/src/app/web-workers/reportes/reporte-personal-credencial-salud.ts
+++ resources/frontend/src/app/web-workers/reportes/reporte-personal-credencial-salud.ts
@@ -98,7 +98,7 @@ export class ReporteTrabajadorCredencialSalud{
           {
             area = element.rel_datos_laborales.cr_fisico.descripcion_actualizada;
           }
-          let margen_tabla = 133;
+          let margen_tabla = 115;
           let margen_imagen = 50;
           let clasificacion = "";
 
@@ -109,7 +109,7 @@ export class ReporteTrabajadorCredencialSalud{
           
           if(iteraccion%2 == 0)
           {
-            margen_tabla = 482;
+            margen_tabla = 466;
             margen_imagen = 400;
             bandera = 1;
           }
@@ -120,31 +120,32 @@ export class ReporteTrabajadorCredencialSalud{
             //margin: [11,margen_tabla,0,0],
             absolutePosition: {x: 49, y: margen_tabla},
             table: {
-              widths: [6, 68, 133, 105, 110],
+              widths: [3, 68, 133, 105, 110],
               
               //heights: [1,8,30/*, 34, 10*/],
               body: [
                 [ { image: formato, width: 444, absolutePosition: {x: 50, y: margen_imagen} }, {},{},{},{}
                 ],
                 
-                [ {},{image: foto, width: 69, height: 83, rowSpan:5, margin:[0.5,4,0,0]},{text:"", margin:[0,3,0,0] },
-                {qr: "https://rhid.saludchiapas.gob.mx/#/ssa/"+element.encriptar, fit: "70", rowSpan:5, margin: [ 20,40,0,0 ]},
-                {image: donador, width: 75, height: 75, alignment: 'center', margin: [ 0,30,10,0 ], rowSpan:5}],
+                [ {},{image: foto, width: 69, height: 83, rowSpan:5, margin:[0,6,0,0]},{text:"", margin:[0,3,0,0] },
+                {qr: "https://rhid.saludchiapas.gob.mx/#/ssa/"+element.encriptar, fit: "70", rowSpan:10, margin: [ 20,60,0,0 ]},
+                {image: donador, width: 75, height: 75, alignment: 'center', margin: [ 0,50,10,0 ], rowSpan:10}
+                
+                ],
                 [{},{},{text: element.nombre+" "+element.apellido_paterno+" "+element.apellido_materno, style:"principal", margin:[0,0,0,1] },{},{}],
                 [{},{},{text:"ID: "+id, style:"subprincipal", margin:[0,0,0,1] },
                 {},
                 {}],
                 [{},{},{text:element.credencial.cargo.descripcion, style:"subprincipal", margin:[0,0,0,1] },{},{}],
-                //[{},{},{text:"VIGENCIA: 31/12/23", style:"vigencia"},{},{}],
-                //[{},{},{text:clasificacion.toUpperCase(), style:"vigencia", margin:[0,2,0,1]},
                 [{},{},{text:"TIPO DE SANGRE: "+tipo_sanguineo[element.credencial.tipo_sanguineo]+" RH "+signo_sanguineo[element.credencial.rh], style:"subprincipal", margin:[0,0,0,1]},
                 {},
                 {}],
-                //[{},{},{text:"TIPO DE SANGRE: "+tipo_sanguineo[element.credencial.tipo_sanguineo]+" RH "+signo_sanguineo[element.credencial.rh], margin:[0,3,0,0], style:"tipo_sangre"},{},{}],
                 [{},{},{},{},{}],
-                //[{ text: "", style: "datos_trabajador", colSpan:3 }, {}, {}, {}, {} ],
-                //[{ text: "", style: "subtitulo", colSpan:3, margin: [ 2,4,2,0 ] }, {}, {}, {}, {} ],
-                //[{ text: clasificacion.toUpperCase(), style: "subtitulo", colSpan:3, margin: [ 0,0,0,0 ] }, {}, {}, {}, {} ],
+                [{},{},{},{},{}],
+                [{},{},{},{},{}],
+                [{},{},{},{},{}],
+                [{},{},{},{},{}],
+                [{},{},{},{},{}],
                 [{ text: area.toUpperCase(), style: "subtitulo", colSpan:3, margin: [ 0,0,0,0 ] }, {}, {}, {}, {} ],
                 [{ text: "CONTACTO DE EMERGENCIA:", style: "contacto", colSpan:3, margin: [ 0,0,0,0 ]}, {}, {}, {}, {} ],
                 [{ text: element.credencial.contacto.toUpperCase(), style: "contacto", colSpan:3 }, {}, {}, {}, {} ],
